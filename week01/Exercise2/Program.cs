87f435b5326ac0b37bd4e0d71625802627198dcf
CadenClawson <darthvaderisboss2@gmail.com>
2025-09-11 23:06:58 -0600

    Update Program.cs
    coded the program to calcutate a letter grade when given the percentage

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        char letterGrade;
+
+        if(gradePercentage >= 90)
+        {
+            letterGrade = 'A';
+        }
+        else if (gradePercentage >= 80);
+        {
+            letterGrade = 'B';
+        }
+        else if (gradePercentage >= 70);
+        {
+            letterGrade = 'C';
+        }
+        else if (gradePercentage >= 60);
+        {
+            letterGrade = 'D';
+        }
+        else
+        {
+            letterGrade = 'F';
+        }
+
+        Console.Writeline("Your letter grade is: {letterGrade}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.Writeline("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.Writeline("Don't worry, you can do better next time. Keep practicing!");
+        }
     }
 }
